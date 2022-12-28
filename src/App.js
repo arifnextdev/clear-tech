27bf50cbb5ac8e39b7be9b4a71f2ccfed6ba612b
ariful171042 <arif171042@gmail.com>
2022-12-28 15:38:25 +0600

    Navber 50% compelet

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
-import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Cart from "./pages/Cart";
+import products from "./pages/products";
+import NotFound from "./pages/NotFound";
+import Navber from "./compoenents/Navber";
+import Slider from "./compoenents/Slider";
+import Footer from "./compoenents/Footer";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <div className="app">
+        <Navber />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/Products" element={<products />} />
+          <Route path="/Cart" element={<Cart />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </div>
+      <Footer />
+    </>
+  );
 };
 
 export default App;
--- src/compoenents/Navber.js
+++ src/compoenents/Navber.js
@@ -1,7 +1,29 @@
-import React from "react";
+import { FiShoppingCart } from "react-icons/fi";
+import { Link } from "react-router-dom";
 
 const Navber = () => {
-  return <div>Navber</div>;
+  return (
+    <div className="navber-bg">
+      <div className="navber">
+        <div className="nav-left">
+          <span>
+            Clear <span>Tech</span>
+          </span>
+        </div>
+        <div className="nav-right">
+          <Link to="/" className="hoem">
+            Home
+          </Link>
+          <Link to="/Products">Products</Link>
+          <Link to="Cart">
+            <span>
+              <FiShoppingCart />
+            </span>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navber;
