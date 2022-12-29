a1b63bd3ed508a2bed2053292ca13cb54dab1afb
ariful171042 <arif171042@gmail.com>
2022-12-29 11:55:25 +0600

    Navber done

--- src/compoenents/Navber.js
+++ src/compoenents/Navber.js
@@ -3,21 +3,27 @@ import { Link } from "react-router-dom";
 
 const Navber = () => {
   return (
-    <div className="navber-bg">
-      <div className="navber">
-        <div className="nav-left">
-          <span>
-            Clear <span>Tech</span>
+    <div className="navbar-bg bg-violet-700 text-violet-50 h-20 flex justify-between items-center ">
+      <div className="navber container mx-auto flex justify-between items-center gap-5">
+        <div className="nav-left cursor-pointer">
+          <span className="logo  text-xl font-semibold">
+            Clear <span className="text-orange-500 ">Tech</span>
           </span>
         </div>
-        <div className="nav-right">
-          <Link to="/" className="hoem">
+        <div className="nav-right flex items-center gap-5 ">
+          <Link to="/" className="nav-links">
             Home
           </Link>
-          <Link to="/Products">Products</Link>
+          <Link to="/Products" className="nav-links">
+            Products
+          </Link>
+
           <Link to="Cart">
-            <span>
+            <span className="cart-icons relative">
               <FiShoppingCart />
+              <span className="cart-container z-[1] absolute -top-3 -right-3 text-xs bg-orange-600 h-5 w-5 rounded-full flex items-center justify-center font-medium ">
+                10
+              </span>
             </span>
           </Link>
         </div>
