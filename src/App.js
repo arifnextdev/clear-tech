d9f95f772bb8dce2d7d6d85ba0fb38dec9fcdc2c
ariful171042 <arif171042@gmail.com>
2023-01-10 15:22:04 +0600

    hero done

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/Products" element={<Products />} />
+          <Route path="/Products/:id" element={<Products />} />
           <Route path="/Cart" element={<Cart />} />
           <Route path="*" element={<NotFound />} />
         </Routes>
--- src/compoenents/Slider.js
+++ src/compoenents/Slider.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { BsArrowLeft, BsArrowRight } from "react-icons/bs";
+import Slide from "./Slide";
 
 const data = [
   {
@@ -66,11 +67,7 @@ const Slider = () => {
         style={{ transform: `translateX(-${100 * currentSlide}vw )` }}
       >
         {data.map((image) => (
-          <div
-            className="slide"
-            style={{ backgroundImage: `url(${image.src})` }}
-            key={image.id}
-          ></div>
+          <Slide key={image.id} image={image} />
         ))}
       </div>
       <div className="btns absolute left-0 right-0 bottom-20 m-auto w-fit z-[1] text-2xl flex gap-10 ">
