0422298ada10b1e32be499a0008b65a1a21b7325
ariful171042 <arif171042@gmail.com>
2023-01-10 20:51:58 +0600

    60%

--- src/compoenents/Navber.js
+++ src/compoenents/Navber.js
@@ -1,16 +1,13 @@
 import { Link } from "react-router-dom";
 import { BsCart3 } from "react-icons/bs";
-// import { useSelector } from "react-redux";
 
 const Navbar = () => {
-  // const { cartItems } = useSelector((state) => state.cart);
-
   return (
     <div className="navbar-bg bg-violet-700 text-violet-50 h-20 flex justify-center items-center">
       <div className="navbar container mx-auto flex items-center justify-between">
         <div className="left">
           <span className="text-xl font-semibold">
-            tech<span className="text-orange-500">Alpha</span>
+            Clear<span className="text-orange-500">Tech</span>
           </span>
         </div>
         <div className="right flex items-center gap-5">
@@ -25,7 +22,6 @@ const Navbar = () => {
               <BsCart3 />
               <span className="cart-counter absolute z-[1] -top-3 -right-3 text-xs bg-orange-600 h-5 w-5 rounded-full flex items-center justify-center font-medium">
                 <p>10</p>
-                {/* {cartItems.length} */}
               </span>
             </span>
           </Link>
